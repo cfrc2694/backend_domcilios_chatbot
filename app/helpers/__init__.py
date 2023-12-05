9ed96f3a724cb713bce6d8be9fd6459711baea6c
Cristian Fernando Rodríguez Cruz <94081091+cfrc2694@users.noreply.github.com>
2023-12-05 02:20:26 +0000

    ADD extract_session_id helper

--- app/helpers/__init__.py
+++ app/helpers/__init__.py
@@ -1,3 +1,4 @@
 from .food_dict_helper import get_str_from_food_dict
+from .session_helper import extract_session_id
 
-__all__ = ["get_str_from_food_dict"]
+__all__ = ["get_str_from_food_dict", "extract_session_id"]
