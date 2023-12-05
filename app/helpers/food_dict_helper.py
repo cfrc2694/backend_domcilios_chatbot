2a049002f51a644438a145132219776323a3d731
Cristian Fernando Rodríguez Cruz <94081091+cfrc2694@users.noreply.github.com>
2023-12-05 02:20:08 +0000

    ADD Short description

--- app/helpers/food_dict_helper.py
+++ app/helpers/food_dict_helper.py
@@ -1,7 +1,8 @@
 """ Helper functions for food_dict module """
 
 
-def get_str_from_food_dict(food_dict: dict):
+def get_str_from_food_dict(food_dict: dict) -> str:
+    """Convert food dict to string"""
     result = ", ".join(
         [f"{int(value)} {key}" for key, value in food_dict.items()]
     )
